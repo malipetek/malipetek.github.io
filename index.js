c072f6358d14471ca33f2aea323f798b90b4b578
muhammet ali petek <malipetek@gmail.com>
2019-09-29 02:49:43 +0300

    line order changed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 window.srequire = async function(url) {
   const relative = /^https?:\/\//i;
   const isRelative = !relative.test(url);
-  url = isRelative ? `${window.location.origin}/${url}` : url;
   url = /src\//.test(url) ? `./src${url.replace(".", "")}` : url;
+  url = isRelative ? `${window.location.origin}/${url}` : url;
   console.log(url);
   const response = await fetch(url);
   let js = await response.text();
