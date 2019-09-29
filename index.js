b214fdf198af93ed6cfc0611fd2b5b89bfff3581
muhammet ali petek <malipetek@gmail.com>
2019-09-29 07:53:50 +0300

    disable wrapping

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ function evalGlobal(js) {
   // we store globals to detect additions later
   var keys = Object.keys(window).slice();
 
-  // we wrap in a function to capture exports
-  var functionid = "import_" + new Date().getTime().toString(36);
-  // replace module exports with return
-  js = js.replace(/module.exports\s*=\s*/gi, "return ");
-  // now wrapping
-  js = "function " + functionid + "(){ " + js + "}";
   // execute and have funtion in global
   eval.call(window, js);
 
