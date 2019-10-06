6aef260250f54b5d06ae61a35200ece289f17b13
muhammet ali petek <malipetek@gmail.com>
2019-10-06 11:44:51 +0300

    clear console log

--- sw.js
+++ sw.js
@@ -4,10 +4,6 @@ const CACHE_MAP = ["/"];
 self.addEventListener("install", async event => {
   const cache = await caches.open(CACHE_NAME);
   cache.addAll(CACHE_MAP);
-
-  const MAINPAGECACHE = await caches.match("/");
-
-  console.log("response from cache", MAINPAGECACHE.bodyUsed);
   // now we cached main mage
 });
 
