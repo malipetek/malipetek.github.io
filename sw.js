ab5394cb47d2acc84ab9fd61b5b1c27bfa86532a
muhammet ali petek <malipetek@gmail.com>
2019-10-06 12:04:20 +0300

    give waituntil promise

--- sw.js
+++ sw.js
@@ -2,29 +2,33 @@ const CACHE_NAME = "githubio-cache";
 const CACHE_MAP = ["/"];
 
 self.addEventListener("install", event => {
-  event.waitUntil(async () => {
-    const cache = await caches.open(CACHE_NAME);
-    cache.addAll(CACHE_MAP);
-    // now we cached main mage
-    console.log("main page cached");
-  });
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(CACHE_MAP);
+      // now we cached main mage
+      console.log("main page cached");
+    })()
+  );
 });
 
 self.addEventListener("fetch", event => {
   // lets make it wait
   console.log("fetch event", event.request.url);
-  event.waitUntil(async () => {
-    const response = await fetch(event.request);
-    console.log("response", response);
-    if (response.ok) {
-      return event.respondWith(response);
-    } else {
-      const MAINPAGECACHE = await caches.match("/");
-      if (MAINPAGECACHE) {
-        return event.respondWith(MAINPAGECACHE.clone());
-      } else {
+  event.waitUntil(
+    (async () => {
+      const response = await fetch(event.request);
+      console.log("response", response);
+      if (response.ok) {
         return event.respondWith(response);
+      } else {
+        const MAINPAGECACHE = await caches.match("/");
+        if (MAINPAGECACHE) {
+          return event.respondWith(MAINPAGECACHE.clone());
+        } else {
+          return event.respondWith(response);
+        }
       }
-    }
-  });
+    })()
+  );
 });
