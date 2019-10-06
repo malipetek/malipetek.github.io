778e7d8f4276020040e39c76de8b1b6070e2494d
muhammet ali petek <malipetek@gmail.com>
2019-10-06 11:57:41 +0300

    added console logs

--- sw.js
+++ sw.js
@@ -1,14 +1,18 @@
 const CACHE_NAME = "githubio-cache";
 const CACHE_MAP = ["/"];
 
-self.addEventListener("install", async event => {
-  const cache = await caches.open(CACHE_NAME);
-  cache.addAll(CACHE_MAP);
-  // now we cached main mage
+self.addEventListener("install", event => {
+  event.waitUntil(async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll(CACHE_MAP);
+    // now we cached main mage
+    console.log("main page cached");
+  });
 });
 
 self.addEventListener("fetch", event => {
   // lets make it wait
+  console.log("fetch event", event.request.url);
   event.waitUntil(async () => {
     const response = await fetch(event.request);
     console.log("response", response);
